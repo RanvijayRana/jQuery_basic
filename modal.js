7ff656defd204ea1db9d9a5d97408bbee64b816d
mr ranvijay <ranvijay.cse@outlook.com>
2018-06-27 07:03:36 +0530

    navbar added

---
+++ modal.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+	//$(".btnArea").click(() => {
+		//$(".screenSize").fadeTo(10, 0.7);
+		//alert("hello World");
+	//});
+	$(".btnArea").click(() => {
+		$(".screenSize").display = "none";
+		alert("hello World");
+	});
+	//$("#overlay").onclick(() => {
+	//	$(".screenSize").style.display = "none";
+	//});
+});
