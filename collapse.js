b394bdc523a551bbc670b4d93bb6a8c42f63c55e
mr ranvijay <ranvijay.cse@outlook.com>
2018-07-15 16:46:36 +0530

    navbar

--- collapse.js
+++ collapse.js
@@ -1,16 +1,32 @@
 $(document).ready(() => {
 	$(".group1").click(() => {
+		let displayType = $(".collSubGroup1").attr("class").css("display");
+		alert(displayType);
+		$(".group1").css("text-decoration","underline");
+		$(".group2").css("text-decoration","none");
+		$(".group3").css("text-decoration","none");
+		/*if(displayType == "flex"){
+			$(".collSubGroup1").css("display", "none");
+		}
+		else (displayType == "none"){
+			$(".collSubGroup1").css("display", "flex");
+		}*/
 		
-		$(".collSubGroup1").css("display", "flex");
 		$(".collSubGroup2").css("display", "none");
 		$(".collSubGroup3").css("display", "none");
 	});
 	$(".group2").click(() => {
+		$(".group1").css("text-decoration","none");
+		$(".group2").css("text-decoration","underline");
+		$(".group3").css("text-decoration","none");
 		$(".collSubGroup1").css("display", "none");
 		$(".collSubGroup2").css("display", "flex");
 		$(".collSubGroup3").css("display", "none");
 	});
 	$(".group3").click(() => {
+		$(".group1").css("text-decoration","none");
+		$(".group2").css("text-decoration","none");
+		$(".group3").css("text-decoration","underline");
 		$(".collSubGroup1").css("display", "none");
 		$(".collSubGroup2").css("display", "none");
 		$(".collSubGroup3").css("display", "flex");
--- navBar.js
+++ navBar.js
@@ -1,11 +1,17 @@
 $(document).ready(() => {
 	$(".menu").click(() => {
-		$(".navContainer").css("height", "60vh");
+		$(".navContainer").css("height", "50vh");
 		$(".navContainer").css("flex-direction", "column");
+		$(".navContainer").css("justify-content","flex-start");
 		$(".home").css("display", "flex");
 		$(".home").css("justify-content", "space-between");
 		$(".home").css("width", "90%");
 		$(".pageTitle").css("width", "90%");
+		$(".section2").css("display", "flex");
+		$(".inputSearch").css("width", "70%");
+		$(".search").css("width", "22%");
 		
 	});
+	
+	
 });
