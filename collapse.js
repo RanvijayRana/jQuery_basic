f1613ea80c506dd62e2ea1b2e1ee41cb0e45e921
mr ranvijay <ranvijay.cse@outlook.com>
2018-07-15 17:07:59 +0530

    collapse click rectified

--- collapse.js
+++ collapse.js
@@ -1,42 +1,58 @@
 $(document).ready(() => {
 	$(".group1").click(() => {
-		let displayType = $(".collSubGroup1").attr("class").css("display");
-		alert(displayType);
-		$(".group1").css("text-decoration","underline");
 		$(".group2").css("text-decoration","none");
 		$(".group3").css("text-decoration","none");
-		/*if(displayType == "flex"){
-			$(".collSubGroup1").css("display", "none");
-		}
-		else (displayType == "none"){
+		
+		let displayType = $(".collSubGroup1").css("display");		
+		if(displayType == "none"){
+			$(".group1").css("text-decoration","underline");
 			$(".collSubGroup1").css("display", "flex");
-		}*/
+		}
+		else if (displayType == "flex"){
+			$(".group1").css("text-decoration","none");
+			$(".collSubGroup1").css("display", "none");
+		}	
 		
 		$(".collSubGroup2").css("display", "none");
 		$(".collSubGroup3").css("display", "none");
 	});
 	$(".group2").click(() => {
 		$(".group1").css("text-decoration","none");
-		$(".group2").css("text-decoration","underline");
 		$(".group3").css("text-decoration","none");
+		
 		$(".collSubGroup1").css("display", "none");
-		$(".collSubGroup2").css("display", "flex");
+		
+		let displayType = $(".collSubGroup2").css("display");		
+		if(displayType == "none"){
+			$(".collSubGroup2").css("display", "flex");
+			$(".group2").css("text-decoration","underline");
+		}
+		else if (displayType == "flex"){
+			$(".group2").css("text-decoration","none");
+			$(".collSubGroup2").css("display", "none");
+		}
+		
 		$(".collSubGroup3").css("display", "none");
 	});
 	$(".group3").click(() => {
 		$(".group1").css("text-decoration","none");
 		$(".group2").css("text-decoration","none");
-		$(".group3").css("text-decoration","underline");
+		
 		$(".collSubGroup1").css("display", "none");
 		$(".collSubGroup2").css("display", "none");
-		$(".collSubGroup3").css("display", "flex");
+		
+		let displayType = $(".collSubGroup3").css("display");
+		if(displayType == "none"){
+			$(".collSubGroup3").css("display", "flex");
+			$(".group3").css("text-decoration","underline");
+		}
+		else if (displayType == "flex"){
+			$(".collSubGroup3").css("display", "none");
+			$(".group3").css("text-decoration","none");
+		}
 	});
 });
 
 
-	//	$(".navContainer").css("flex-direction", "column");
-	//	$(".home").css("display", "flex");
-	//	$(".home").css("justify-content", "space-between");
-		//$(".home").css("width", "90%");
-		//$(".pageTitle").css("width", "90%");
+	
 		
