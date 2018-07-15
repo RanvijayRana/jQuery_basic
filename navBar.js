c665fff7b9fdb9254a16b1fed77e687dbb407e29
mr ranvijay <ranvijay.cse@outlook.com>
2018-07-15 23:50:53 +0530

    navbar js logic modified

--- navBar.js
+++ navBar.js
@@ -1,16 +1,23 @@
 $(document).ready(() => {
 	$(".menu").click(() => {
-		$(".navContainer").css("height", "50vh");
-		$(".navContainer").css("flex-direction", "column");
-		$(".navContainer").css("justify-content","flex-start");
-		$(".home").css("display", "flex");
-		$(".home").css("justify-content", "space-between");
-		$(".home").css("width", "90%");
-		$(".pageTitle").css("width", "90%");
-		$(".section2").css("display", "flex");
-		$(".inputSearch").css("width", "70%");
-		$(".search").css("width", "22%");
-		
+		let indicator = $(".home").css("display");
+		if(indicator == "none"){
+			$(".navContainer").css("height", "51vh");
+			$(".navContainer").css("flex-direction", "column");
+			$(".navContainer").css("justify-content","flex-start");
+			$(".home").css("display", "flex");
+			$(".home").css("justify-content", "space-between");
+			$(".home").css("width", "90%");
+			$(".pageTitle").css("width", "90%");
+			$(".section2").css("display", "flex");
+			$(".inputSearch").css("width", "70%");
+			$(".search").css("width", "22%");
+		}
+		else{
+			$(".home").css("display", "none");
+			$(".section2").css("display", "none");
+			$(".navContainer").css("height", "10vh");
+		}
 	});
 	
 	
