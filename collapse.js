7d184446d6da3d6adc66d2baf805817bd53920c1
mr ranvijay <ranvijay.cse@outlook.com>
2018-07-15 23:00:56 +0530

    final change

--- collapse.js
+++ collapse.js
@@ -3,6 +3,10 @@ $(document).ready(() => {
 		$(".group2").css("text-decoration","none");
 		$(".group3").css("text-decoration","none");
 		
+		
+		$(".collGroup3").css("border-bottom-left-radius","10px");
+		$(".collGroup3").css("border-bottom-right-radius","10px");
+		
 		let displayType = $(".collSubGroup1").css("display");		
 		if(displayType == "none"){
 			$(".group1").css("text-decoration","underline");
@@ -19,6 +23,10 @@ $(document).ready(() => {
 	$(".group2").click(() => {
 		$(".group1").css("text-decoration","none");
 		$(".group3").css("text-decoration","none");
+		
+		
+		$(".collGroup3").css("border-bottom-left-radius","10px");
+		$(".collGroup3").css("border-bottom-right-radius","10px");
 		
 		$(".collSubGroup1").css("display", "none");
 		
@@ -45,10 +53,16 @@ $(document).ready(() => {
 		if(displayType == "none"){
 			$(".collSubGroup3").css("display", "flex");
 			$(".group3").css("text-decoration","underline");
+			$(".collGroup3").css("border-bottom-left-radius","0px");
+			$(".collGroup3").css("border-bottom-right-radius","0px");
+			$(".collSubGroup3").css("border-bottom-left-radius","10px");
+			$(".collSubGroup3").css("border-bottom-right-radius","10px");
 		}
 		else if (displayType == "flex"){
 			$(".collSubGroup3").css("display", "none");
 			$(".group3").css("text-decoration","none");
+			$(".collGroup3").css("border-bottom-left-radius","10px");
+			$(".collGroup3").css("border-bottom-right-radius","10px");
 		}
 	});
 });
